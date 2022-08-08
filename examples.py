53b744763a40639c595b5175b90608ae5a1c1a60
imamirhossein <amirhosseinkhoshdel@gmail.com>
2022-08-08 12:32:49 +0430

    feat: :fire: added get_gender_failed to examples

--- examples.py
+++ examples.py
@@ -4,8 +4,8 @@ from request_handler import RequestHandler
 def get_gender_success():
     url = "https://api.genderize.io/"
     payload = {
-            "name": "amir"
-        }
+        "name": "amir"
+    }
 
     cls = RequestHandler()
     response = cls.send(url=url, method="get", expected_status=200, raise_exception=True, headers={}, params=payload)
@@ -13,3 +13,15 @@ def get_gender_success():
         return f"status: {response.status_code} and json: {response.json()}"
     cls.raise_invalid_status(response=response)
 
+
+def get_gender_failed():
+    url = "https://api.genderize.io/"
+    payload = {
+        "name": "john"
+    }
+
+    cls = RequestHandler()
+    response = cls.send(url=url, method="get", expected_status=200, raise_exception=True, headers={}, json=payload)
+    if response.status_code == 200:
+        return f"status: {response.status_code} and json: {response.json()}"
+    cls.raise_invalid_status(response=response)
